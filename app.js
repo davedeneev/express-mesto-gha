613e22beef8e407911c665224f9ac96a95657e2f
Dmitry Vedeneev <davedeneev@yandex.ru>
2023-10-03 02:55:53 +0300

    Пр13 Первая итерация

--- app.js
+++ app.js
@@ -22,8 +22,6 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => {
-  res.status(404).send({ message: 'Страница не найдена' });
-});
+
 
 app.listen(PORT);
